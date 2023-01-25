46c7d0b01eac0186e1abe674a187ab451b5ebac7
Luis Ramos <ljralonso@gmail.com>
2023-01-25 16:56:28 -0400

    wait for gigs to load

--- src/pages/gigs/index.js
+++ src/pages/gigs/index.js
@@ -13,17 +13,20 @@ import { getGigs } from 'hooks/gigs';
 const Gigs = () => {
   const theme = useTheme();
   const { user } = useAuth();
-  const { data } = useQuery(['gigs'], () => getGigs({ userId: user.id }));
-  console.log('data: ', data);
+  const { data: gigs, isLoading } = useQuery(['gigs'], () => getGigs({ userId: user.id }));
+  if (isLoading) {
+    return <div>Loading Gigs...</div>;
+  }
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {data.map((gig) => {
-        return (
-          <Grid item xs={12} lg={4} sm={6} key={gig.id}>
-            <HoverSocialCard primary={gig.price} secondary={gig.name} iconPrimary={RiseOutlined} color={theme.palette.primary.main} />
-          </Grid>
-        );
-      })}
+      {gigs &&
+        gigs.map((gig) => {
+          return (
+            <Grid item xs={12} lg={4} sm={6} key={gig.id}>
+              <HoverSocialCard primary={gig.price} secondary={gig.name} iconPrimary={RiseOutlined} color={theme.palette.primary.main} />
+            </Grid>
+          );
+        })}
     </Grid>
   );
 };
