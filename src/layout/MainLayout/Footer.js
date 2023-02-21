d3bf4ba63bab0d97c12e5c5e12cea9be0c4659a5
Luis Ramos <ljralonso@gmail.com>
2023-02-21 15:19:55 -0400

    Privacy link

--- src/layout/MainLayout/Footer.js
+++ src/layout/MainLayout/Footer.js
@@ -7,7 +7,12 @@ const Footer = () => (
   <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
     <Typography variant="caption">2023 &copy; OneGig. All rights reserved</Typography>
     <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
-      <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
+      <Link
+        href="https://app.termly.io/document/privacy-policy/70a437d1-cbf8-482d-89a7-db3b09df80de"
+        target="_blank"
+        variant="caption"
+        color="textPrimary"
+      >
         Privacy
       </Link>
       <Link component={RouterLink} to="#" target="_blank" variant="caption" color="textPrimary">
